a12b034558e53aeece3db7929aed45fb65c25f86
Elic <rebellious.licxz@gmail.com>
2016-12-14 09:22:24 +0700

    Install ng2-table , ng2-bootstrap , and jquery plugins

--- app/modules/app.module.ts
+++ app/modules/app.module.ts
@@ -1,10 +1,24 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }                     from '@angular/core';
+import { BrowserModule }                from '@angular/platform-browser';
+import { FormsModule }  				from '@angular/forms';
+import { ReactiveFormsModule }			from '@angular/forms';
+import { HttpModule }                   from '@angular/http';
+import { Ng2TableModule }               from 'ng2-table/ng2-table';
+import { Ng2BootstrapModule }           from 'ng2-bootstrap/ng2-bootstrap';
+import { PaginationModule }             from 'ng2-bootstrap/ng2-bootstrap';
 
-import { AppComponent }  from '../components/app.component';
+import { AppComponent }  				from '../components/app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ 
+  	BrowserModule,
+  	FormsModule,
+  	ReactiveFormsModule,
+  	HttpModule,
+  	Ng2TableModule,
+  	Ng2BootstrapModule,
+  	PaginationModule,
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
