bbc7f4f7bb3187561a1b13153c71aca3e8878886
Elic <rebellious.licxz@gmail.com>
2016-12-15 16:47:25 +0700

    Added contractor CRUD function in service

--- dist/app/services/contractor.service.js
+++ dist/app/services/contractor.service.js
@@ -10,11 +10,45 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require("@angular/core");
 var http_1 = require("@angular/http");
-// import { User } from '../models/index';
+var Rx_1 = require("rxjs/Rx");
 var ContractorService = (function () {
     function ContractorService(http) {
         this.http = http;
     }
+    ContractorService.prototype.getAll = function () {
+        return this.http.get('https://192.168.10.73:3333/api/contractors')
+            .map(function (res) { return res.json(); })
+            .catch(function (error) { return Rx_1.Observable.throw(error.json().error || 'Server error'); });
+    };
+    ContractorService.prototype.getById = function (id) {
+        return this.http.get('https://192.168.10.73:3333/api/contractors' + id)
+            .map(function (res) { return res.json(); })
+            .catch(function (error) { return Rx_1.Observable.throw(error.json().error || 'Server error'); });
+    };
+    ContractorService.prototype.create = function (body) {
+        var options = new http_1.RequestOptions({
+            headers: new http_1.Headers({ 'Content-Type': 'application/json;charset=UTF-8' })
+        });
+        return this.http.post('https://192.168.10.73:3333/api/contractors', body, options)
+            .map(function (res) { return res.json(); })
+            .catch(function (error) { return Rx_1.Observable.throw(error.json().error || 'Server error'); });
+    };
+    ContractorService.prototype.update = function (body) {
+        var options = new http_1.RequestOptions({
+            headers: new http_1.Headers({ 'Content-Type': 'application/json;charset=UTF-8' })
+        });
+        return this.http.put('https://192.168.10.73:3333/api/contractors' + body._id, body, options)
+            .map(function (res) { return res.json(); })
+            .catch(function (error) { return Rx_1.Observable.throw(error.json().error || 'Server error'); });
+    };
+    ContractorService.prototype.delete = function (id) {
+        var options = new http_1.RequestOptions({
+            headers: new http_1.Headers({ 'Content-Type': 'application/json;charset=UTF-8' })
+        });
+        return this.http.delete('https://192.168.10.73:3333/api/contractors' + id, options)
+            .map(function (res) { return res.json(); })
+            .catch(function (error) { return Rx_1.Observable.throw(error.json().error || 'Server error'); });
+    };
     return ContractorService;
 }());
 ContractorService = __decorate([
