c875355e47a9f440d21174c989223c2ad25008b7
Elic <rebellious.licxz@gmail.com>
2016-12-15 16:48:15 +0700

    Added facility CRUD function in service

--- app/services/facility.service.ts
+++ app/services/facility.service.ts
@@ -1,40 +1,48 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
- 
-// import { User } from '../models/index';
+import { Observable } from 'rxjs/Rx';
+import { Facility } from '../models/index';
  
 @Injectable()
 export class FacilityService {
     constructor(private http: Http) {}
- 
-    // getAll() {
-    //     return this.http.get('https://192.168.10.73:3333/api/users',  this.jwt()).map((response: Response) => response.json());
-    // }
- 
-    // getById(id: number) {
-    //     return this.http.get('https://192.168.10.73:3333/api/users' + id, this.jwt()).map((response: Response) => response.json());
-    // }
- 
-    // create(user: User) {
-    //     return this.http.post('https://192.168.10.73:3333/api/users', user, this.jwt()).map((response: Response) => console.log(response.json()));
-    // }
- 
-    // update(user: User) {
-    //     return this.http.put('https://192.168.10.73:3333/api/users' + user.username, user, this.jwt()).map((response: Response) => response.json());
-    // }
- 
-    // delete(id: number) {
-    //     return this.http.delete('https://192.168.10.73:3333/api/users' + id, this.jwt()).map((response: Response) => response.json());
-    // }
- 
-    // // private helper methods
- 
-    // private jwt() {
-    //     // create authorization header with jwt token
-    //     let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-    //     if (currentUser && currentUser.token) {
-    //         let headers = new Headers({ 'Authorization': 'Bearer ' + currentUser.token });
-    //         return new RequestOptions({ headers: headers });
-    //     }
-    // }
+
+    getAll(){
+        return this.http.get('https://192.168.10.73:3333/api/facilities')
+            .map((res:Response) => res.json())
+            .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    }
+
+    getById(id:string){
+        return this.http.get('https://192.168.10.73:3333/api/facilities' + id)
+            .map((res:Response) => res.json())
+            .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    }
+
+    create(body:Facility){
+        let options = new RequestOptions({
+            headers: new Headers({ 'Content-Type': 'application/json;charset=UTF-8' }) 
+        });
+        return this.http.post('https://192.168.10.73:3333/api/facilities',body, options)
+            .map((res:Response) => res.json())
+            .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    }
+
+    update(body:Facility){
+        let options = new RequestOptions({
+            headers: new Headers({ 'Content-Type': 'application/json;charset=UTF-8' }) 
+        });
+        return this.http.put('https://192.168.10.73:3333/api/facilities' + body._id,body, options)
+            .map((res:Response) => res.json())
+            .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    }
+
+    delete(id:string){
+        let options = new RequestOptions({
+            headers: new Headers({ 'Content-Type': 'application/json;charset=UTF-8' }) 
+        });
+        return this.http.delete('https://192.168.10.73:3333/api/facilities' + id, options)
+            .map((res:Response) => res.json())
+            .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    }
 }
