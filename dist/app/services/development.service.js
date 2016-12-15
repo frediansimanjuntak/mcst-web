f3d11ed01ce1c6ccc96122eda01bd6e2ae120153
Elic <rebellious.licxz@gmail.com>
2016-12-15 16:47:46 +0700

    Added development CRUD function in service

--- dist/app/services/development.service.js
+++ dist/app/services/development.service.js
@@ -10,11 +10,45 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require("@angular/core");
 var http_1 = require("@angular/http");
-// import { User } from '../models/index';
+var Rx_1 = require("rxjs/Rx");
 var DevelopmentService = (function () {
     function DevelopmentService(http) {
         this.http = http;
     }
+    DevelopmentService.prototype.getAll = function () {
+        return this.http.get('https://192.168.10.73:3333/api/developments')
+            .map(function (res) { return res.json(); })
+            .catch(function (error) { return Rx_1.Observable.throw(error.json().error || 'Server error'); });
+    };
+    DevelopmentService.prototype.getById = function (id) {
+        return this.http.get('https://192.168.10.73:3333/api/developments' + id)
+            .map(function (res) { return res.json(); })
+            .catch(function (error) { return Rx_1.Observable.throw(error.json().error || 'Server error'); });
+    };
+    DevelopmentService.prototype.create = function (body) {
+        var options = new http_1.RequestOptions({
+            headers: new http_1.Headers({ 'Content-Type': 'application/json;charset=UTF-8' })
+        });
+        return this.http.post('https://192.168.10.73:3333/api/developments', body, options)
+            .map(function (res) { return res.json(); })
+            .catch(function (error) { return Rx_1.Observable.throw(error.json().error || 'Server error'); });
+    };
+    DevelopmentService.prototype.update = function (body) {
+        var options = new http_1.RequestOptions({
+            headers: new http_1.Headers({ 'Content-Type': 'application/json;charset=UTF-8' })
+        });
+        return this.http.put('https://192.168.10.73:3333/api/developments' + body._id, body, options)
+            .map(function (res) { return res.json(); })
+            .catch(function (error) { return Rx_1.Observable.throw(error.json().error || 'Server error'); });
+    };
+    DevelopmentService.prototype.delete = function (id) {
+        var options = new http_1.RequestOptions({
+            headers: new http_1.Headers({ 'Content-Type': 'application/json;charset=UTF-8' })
+        });
+        return this.http.delete('https://192.168.10.73:3333/api/developments' + id, options)
+            .map(function (res) { return res.json(); })
+            .catch(function (error) { return Rx_1.Observable.throw(error.json().error || 'Server error'); });
+    };
     return DevelopmentService;
 }());
 DevelopmentService = __decorate([
