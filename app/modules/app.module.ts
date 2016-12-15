b60b482726d337343c54bfa6cb324dcc602e6617
Gunama Putra <gunama12@gmail.com>
2016-12-15 11:42:01 +0700

    Make Navbar Menu Dynamic/Array

--- app/modules/app.module.ts
+++ app/modules/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule, Directive }          from '@angular/core';
 import { BrowserModule }                from '@angular/platform-browser';
-import { FormsModule }  				from '@angular/forms';
-import { ReactiveFormsModule }			from '@angular/forms';
+import { FormsModule }  				        from '@angular/forms';
+import { ReactiveFormsModule }			    from '@angular/forms';
 import { HttpModule }                   from '@angular/http';
 import { Ng2TableModule }               from 'ng2-table/ng2-table';
 import { Ng2BootstrapModule }           from 'ng2-bootstrap/ng2-bootstrap';
 import { PaginationModule }             from 'ng2-bootstrap/ng2-bootstrap';
-import { AppRoutingModule }     		from './app-routing.module';
+import { AppRoutingModule }     		    from './app-routing.module';
 
 import { 
   AppComponent,
