8b900b18d65000e677425727cb35981f8c5bd531
Elic <rebellious.licxz@gmail.com>
2016-12-19 09:24:26 +0700

    Created newsletter service

---
+++ app/services/newsletter.service.ts
@@ -0,0 +1,48 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { Development } from '../models/index';
+ 
+@Injectable()
+export class DevelopmentService {
+    constructor(private http: Http) {}
+
+    getAll(){
+        return this.http.get('https://192.168.10.73:3333/api/newsletters')
+            .map((res:Response) => res.json())
+            .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    }
+
+    getById(id:string){
+        return this.http.get('https://192.168.10.73:3333/api/newsletters' + id)
+            .map((res:Response) => res.json())
+            .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    }
+
+    create(body:Development){
+        let options = new RequestOptions({
+            headers: new Headers({ 'Content-Type': 'application/json;charset=UTF-8' }) 
+        });
+        return this.http.post('https://192.168.10.73:3333/api/newsletters',body, options)
+            .map((res:Response) => res.json())
+            .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    }
+
+    update(body:Development){
+        let options = new RequestOptions({
+            headers: new Headers({ 'Content-Type': 'application/json;charset=UTF-8' }) 
+        });
+        return this.http.put('https://192.168.10.73:3333/api/newsletters' + body._id,body, options)
+            .map((res:Response) => res.json())
+            .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    }
+
+    delete(id:string){
+        let options = new RequestOptions({
+            headers: new Headers({ 'Content-Type': 'application/json;charset=UTF-8' }) 
+        });
+        return this.http.delete('https://192.168.10.73:3333/api/newsletters' + id, options)
+            .map((res:Response) => res.json())
+            .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    }
+}
