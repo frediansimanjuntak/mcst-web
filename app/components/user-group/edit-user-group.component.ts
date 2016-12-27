b6e6171f5d53086de4612b1fdedf4b9f52824eb3
Gunama Putra <gunama12@gmail.com>
2016-12-27 13:28:18 +0700

    Fix Edit-user-group

--- app/components/user-group/edit-user-group.component.ts
+++ app/components/user-group/edit-user-group.component.ts
@@ -15,6 +15,8 @@ export class EditUserGroupComponent implements OnInit {
 	userGroup: UserGroup;
     model: any = {};
     id: string;
+    myForm: FormGroup;
+    users: User;
 
     constructor(private router: Router,
     	private userGroupService: UserGroupService,
@@ -23,5 +25,57 @@ export class EditUserGroupComponent implements OnInit {
     	private formbuilder: FormBuilder,
         private route: ActivatedRoute,) {}
 
+    ngOnInit(): void {
+        this.myForm = this.formbuilder.group({
+            description : ['', Validators.required],
+            chief : ['', Validators.required],
+            users: this.formbuilder.array([]),
+        });
 
+
+
+        let self = this; 
+        this.userService.getAll()
+            .subscribe(user => {
+                self.users = user;
+                console.log(user);
+            });
+
+        
+        
+        if( this.id != null) {
+            this.userService.getById(this.id).subscribe(user => {this.users = user;console.log(user);});
+        };
+        
+        this.addUser();
+        
+        // this.developmentService.getAll().subscribe(developments => { this.developments = developments; });
+    }
+
+    initUser() {
+        return this.formbuilder.group({});
+    }
+
+    addUser() {
+        const control = <FormArray>this.myForm.controls['users'];
+        const addrCtrl = this.initUser();
+        
+        control.push(addrCtrl);
+        
+        /* subscribe to individual address value changes */
+        // addrCtrl.valueChanges.subscribe(x => {
+        //   console.log(x);
+        // })
+    }
+
+    removeUser(i: number) {
+        const control = <FormArray>this.myForm.controls['users'];
+        control.removeAt(i);
+    }
+
+    save(model: any) {
+        // call API to save
+        // ...
+        console.log(model);
+    }
 }
