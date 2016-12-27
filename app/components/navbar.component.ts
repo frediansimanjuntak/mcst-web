b7bd395ebd25f9c235e27dbb7406d90011f7dcf5
Gunama Putra <gunama12@gmail.com>
2016-12-27 10:15:53 +0700

    Fix Styles URL forcomponent

--- app/components/navbar.component.ts
+++ app/components/navbar.component.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { MENUS } from '../models/menu';
 
 @Component({
-	moduleId: module.id,
+	moduleId: module.id.replace("/dist/", "/"),
 	selector: 'navbar',
 	templateUrl: '/app/templates/navbar.html',
-	styleUrls: [ '/app/templates/styles/navbar.css' ]
+	styleUrls: [ '../templates/styles/navbar.css' ]
 })
 
 export class NavbarComponent {
