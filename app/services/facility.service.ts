4ee84a24f9284e294a6999ed081b0364676c9f4a
Elic <rebellious.licxz@gmail.com>
2017-01-17 14:45:26 +0700

    Fixed service to pass token in the header

--- app/services/facility.service.ts
+++ app/services/facility.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import { Facility, Facilities } from '../models/index';
+import { AuthenticationService } from '../services/index';
 import { url } from '../global';
 import 'rxjs/add/operator/toPromise';
  
 @Injectable()
 export class FacilityService {
-    private headers = new Headers({'Content-Type': 'application/json'});
-    constructor(private http: Http) {}
+    private headers = new Headers({ 'Authorization': 'Bearer ' + this.authenticationService.token });
+    private options = new RequestOptions({ headers: this.headers });
+    constructor(private http: Http, private authenticationService: AuthenticationService) {}
 
     getFacilities(): Promise<Facility[]> {
         return Promise.resolve(Facilities);
@@ -20,40 +22,40 @@ export class FacilityService {
     }
 
     getAll(){
-        return this.http.get(url + 'api/facilities')
+        return this.http.get(url + 'api/facilities', this.options)
             .map((res:Response) => res.json())
             .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
     }
 
     getById(id:string){
-        return this.http.get(url + 'api/facilities/' + id)
+        return this.http.get(url + 'api/facilities/' + id, this.options)
             .map((res:Response) => res.json())
             .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
     }
 
     create(body:any): Promise<Facility> {
-        return this.http.post(url +  'api/facilities', body, {headers: this.headers})
+        return this.http.post(url +  'api/facilities', body, this.options)
             .toPromise()
             .then(res => res.json().data)
             .catch(this.handleError);
     }
 
     update(body:Facility): Promise<Facility> {
-        return this.http.post(url + 'api/facilities/update/' + body._id,body, {headers: this.headers})
+        return this.http.post(url + 'api/facilities/update/' + body._id,body, this.options)
             .toPromise()
             .then(res => res.json().data)
             .catch(this.handleError);
     }
 
     delete(id: string): Promise<void> {
-    return this.http.delete(url + 'api/facilities/' + id, {headers: this.headers})
+    return this.http.delete(url + 'api/facilities/' + id, this.options)
       .toPromise()
       .then(() => null)
       .catch(this.handleError);
     }
 
     private handleError(error: any): Promise<any> {
-        console.error('An error occurred', error); // for demo purposes only
+        console.error('An error occurred', error);
         return Promise.reject(error.message || error);
     }
 }
--- app/services/payment.service.ts
+++ app/services/payment.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { Http, Headers, RequestOptions, Response } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import { Payment, Payments } from '../models/index';
+import { AuthenticationService } from '../services/index';
 import { url } from '../global';
 import 'rxjs/add/operator/toPromise';
  
 @Injectable()
 export class PaymentService {
-    private headers = new Headers({'Content-Type': 'application/json'});
-    constructor(private http: Http) {}
+    private headers = new Headers({ 'Authorization': 'Bearer ' + this.authenticationService.token });
+    private options = new RequestOptions({ headers: this.headers });
+    constructor(private http: Http, private authenticationService: AuthenticationService) {}
 
     getPayments(): Promise<Payment[]> {
         return Promise.resolve(Payments);
@@ -20,41 +22,41 @@ export class PaymentService {
     }
 
     getAll(){
-        return this.http.get(url + 'api/payments')
+        return this.http.get(url + 'api/payments', this.options)
             .map((res:Response) => res.json())
             .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
     }
 
     getById(id:string){
-        return this.http.get(url + 'api/payments/' + id)
+        return this.http.get(url + 'api/payments/' + id, this.options)
             .map((res:Response) => res.json())
             .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
     }
 
     create(body:any): Promise<Payment> {
         console.log(body);
-        return this.http.post(url +  'api/payments', JSON.stringify(body), {headers: this.headers})
+        return this.http.post(url +  'api/payments', JSON.stringify(body), this.options)
             .toPromise()
             .then(res => res.json().data)
             .catch(this.handleError);
     }
 
     update(body:Payment): Promise<Payment> {
-        return this.http.post(url + 'api/payments/update/' + body._id,body, {headers: this.headers})
+        return this.http.post(url + 'api/payments/update/' + body._id,body, this.options)
             .toPromise()
             .then(res => res.json().data)
             .catch(this.handleError);
     }
 
     delete(id: string): Promise<void> {
-    return this.http.delete(url + 'api/payments/' + id, {headers: this.headers})
+    return this.http.delete(url + 'api/payments/' + id, this.options)
       .toPromise()
       .then(() => null)
       .catch(this.handleError);
     }
 
     private handleError(error: any): Promise<any> {
-        console.error('An error occurred', error); // for demo purposes only
+        console.error('An error occurred', error);
         return Promise.reject(error.message || error);
     }
 
