05d2ad38e2e75cf7e647618e94c73fa43aa2ecd5
Gunama Putra <gunama12@gmail.com>
2017-01-20 17:46:03 +0700

    Delete ng2-table

--- app/modules/app.module.ts
+++ app/modules/app.module.ts
@@ -7,7 +7,6 @@ import { SimpleNotificationsModule }    from 'angular2-notifications';
 import { FileDropDirective }            from 'ng2-file-upload';
 import { ReactiveFormsModule }			    from '@angular/forms';
 import { HttpModule }                   from '@angular/http';
-import { Ng2TableModule }               from 'ng2-table/ng2-table';
 import { Ng2BootstrapModule }           from 'ng2-bootstrap/ng2-bootstrap';
 import { PaginationModule,DatepickerModule }             from 'ng2-bootstrap/ng2-bootstrap';
 import { SelectModule }                 from 'ng2-select/ng2-select';
