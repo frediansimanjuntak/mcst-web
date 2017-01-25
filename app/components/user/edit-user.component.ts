74109a50c783d60f25c6a40894dc7571fce9d1d1
Elic <rebellious.licxz@gmail.com>
2017-01-25 10:38:58 +0700

    FIxed edit user component

--- app/components/user/edit-user.component.ts
+++ app/components/user/edit-user.component.ts
@@ -57,8 +57,8 @@ export class EditUserComponent implements OnInit {
                 development: [''],
                 property: ['']
             }),
-            owned_property: this.formbuilder.array([]),
-            authorized_property: this.formbuilder.array([]),
+            owned_property: this.formbuilder.array([this.initOwned()]),
+            authorized_property: this.formbuilder.array([this.initAuthorized()]),
             active: ['', Validators.required],
             default_development: [''],
             authorized_development: ['']
@@ -98,8 +98,8 @@ export class EditUserComponent implements OnInit {
                         development: [''],
                         property: ['']
                     }),
-                    owned_property: this.formbuilder.array([this.initOwned()]),
-                    authorized_property: this.formbuilder.array([this.initAuthorized()]),
+                    owned_property: this.formbuilder.array([]),
+                    authorized_property: this.formbuilder.array([]),
                     active: ['', Validators.required],
                     default_development: [''],
                     authorized_development: [''],
