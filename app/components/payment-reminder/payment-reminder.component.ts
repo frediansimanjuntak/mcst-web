75f9803108723bd5204d32e94311320e04f2798c
Elic <rebellious.licxz@gmail.com>
2017-01-25 09:25:45 +0700

    Created Payment reminder

---
+++ app/components/payment-reminder/payment-reminder.component.ts
@@ -0,0 +1,99 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, Params, ActivatedRoute } from '@angular/router';
+import { PaymentReminder } from '../../models/index';
+import { PaymentReminderService, AlertService, UserService } from '../../services/index';
+import '../../rxjs-operators';
+import { Observable } from 'rxjs/Observable';
+import { FileUploader } from 'ng2-file-upload';
+
+@Component({
+    // moduleId: module.id,
+    selector: 'payment-reminder',
+    templateUrl: 'app/templates/payment-reminder.html',
+})
+
+export class PaymentReminderComponent implements OnInit {
+	paymentreminder: PaymentReminder;
+    paymentreminders: PaymentReminder[] = [];
+    model: any = {};
+    id: string;
+    name: any;
+    draft: any;
+    published: any;
+
+    constructor(private router: Router, 
+        private paymentreminderService: PaymentReminderService, 
+        private alertService: AlertService,
+        private route: ActivatedRoute,
+        private userService: UserService) {}
+
+    ngOnInit(): void {
+        this.userService.getByToken().subscribe(name => {this.name = name;})
+        this.route.params.subscribe(params => {
+            this.id = params['id'];
+        });
+        if( this.id == null) {
+            this.loadAllPaymentReminder();
+        }else{
+        	this.paymentreminderService.getPaymentReminder(this.id).then(paymentreminder => {this.paymentreminder = paymentreminder;});
+        }
+    }
+
+    deletePaymentReminder(paymentreminder: PaymentReminder) {
+    	console.log(paymentreminder._id)
+        this.paymentreminderService.delete(paymentreminder._id)
+          .then(
+            response => {
+              if(response) {
+                console.log(response);
+                // console.log(response.error());
+                alert(`The Newsletter could not be deleted, server Error.`);
+              } else {
+                this.alertService.success('Create user successful', true);
+                alert(`Delete Newsletter successful`);
+                this.ngOnInit()
+              }
+            },
+            error=> {
+              console.log(error);
+                alert(`The Newsletter could not be deleted, server Error.`);
+            }
+        );
+    }
+
+	private loadAllPaymentReminder() {
+		this.paymentreminderService.getPaymentReminders()
+        .then(paymentreminders => {
+            this.published = paymentreminders.filter(data => data.publish == true );
+            this.draft   = paymentreminders.filter(data => data.publish == false );
+            console.log(this.published)
+            for (var i = 0; i < this.published.length; ++i) {
+                this.published[i].auto_issue_on = this.published[i].auto_issue_on.slice(0,10);
+                this.published[i].due_on = this.published[i].due_on.slice(0,10);
+                this.published[i].created_at = this.published[i].created_at.slice(0,10);
+            }
+            for (var i = 0; i < this.draft.length; ++i) {
+                this.draft[i].auto_issue_on = this.draft[i].auto_issue_on.slice(0,10);
+                this.draft[i].due_on = this.draft[i].due_on.slice(0,10);
+                this.draft[i].created_at = this.draft[i].created_at.slice(0,10);
+            }
+        });
+    }
+
+    // edit(incident: Incident){
+    //     this.router.navigate(['/incident/edit', incident._id]);
+    // }
+
+    edit(paymentreminder: PaymentReminder){
+        this.router.navigate([this.name.default_development.name + '/payment_system/edit', paymentreminder._id]);
+    }
+
+    add(){
+        this.router.navigate([this.name.default_development.name + '/payment_system/add']);
+    }
+
+    publish(paymentreminder:PaymentReminder){
+        this.paymentreminderService.publish(paymentreminder._id);
+        this.ngOnInit()
+    }
+}
---
+++ app/services/payment-reminder.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { PaymentReminder, PaymentReminders } from '../models/index';
+import { AuthenticationService } from '../services/index';
+import { url } from '../global';
+import 'rxjs/add/operator/toPromise';
+ 
+@Injectable()
+export class PaymentReminderService {
+    private headers = new Headers({ 'Authorization': 'Bearer ' + this.authenticationService.token });
+    private options = new RequestOptions({ headers: this.headers });
+    constructor(private http: Http, private authenticationService: AuthenticationService) {}
+
+    getPaymentReminders(): Promise<PaymentReminder[]> {
+        return Promise.resolve(PaymentReminders);
+    }
+
+    getPaymentReminder(id: string): Promise<PaymentReminder> {
+        return this.getPaymentReminders()
+            .then(payments => payments.find(payment => payment._id === id));
+    }
+
+    getAll(){
+        return this.http.get(url + 'api/payments', this.options)
+            .map((res:Response) => res.json())
+            .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    }
+
+    getById(id:string){
+        return this.http.get(url + 'api/payments/' + id, this.options)
+            .map((res:Response) => res.json())
+            .catch((error:any) => Observable.throw(error.json().error || 'Server error'));
+    }
+
+    create(body:any): Promise<PaymentReminder> {
+        console.log(body);
+        return this.http.post(url +  'api/payments', JSON.stringify(body), this.options)
+            .toPromise()
+            .then(res => res.json().data)
+            .catch(this.handleError);
+    }
+
+    update(body:PaymentReminder): Promise<PaymentReminder> {
+        return this.http.post(url + 'api/payments/update/' + body._id,body, this.options)
+            .toPromise()
+            .then(res => res.json().data)
+            .catch(this.handleError);
+    }
+
+    delete(id: string): Promise<void> {
+        return this.http.delete(url + 'api/payments/' + id, this.options)
+            .toPromise()
+            .then(() => null)
+            .catch(this.handleError);
+    }
+
+    publish(id: string): Promise<PaymentReminder> {
+        return this.http.post(url + 'api/feedback/publish/' + id,'', this.options)
+            .toPromise()
+            .then(res => res.json().data)
+            .catch(this.handleError);
+    }
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error);
+        return Promise.reject(error.message || error);
+    }
+
+}
