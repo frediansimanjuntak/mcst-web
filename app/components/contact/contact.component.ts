9bc216e2a4b52bd9717e374f6295e241820674af
Gunama Putra <gunama12@gmail.com>
2017-02-16 13:15:47 +0700

    Add contact directory component

---
+++ app/components/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import '../../rxjs-operators';
+
+@Component({
+  moduleId: module.id,
+  selector: 'contact',
+  template: ``,
+})
+
+export class ContactComponent  { 
+	
+}
