a53db7afb26f1d3f941ca4ec862fcc74fd41e0be
Elic <rebellious.licxz@gmail.com>
2017-02-22 12:03:34 +0700

    Fixed loading

--- app/components/token.component.ts
+++ app/components/token.component.ts
@@ -6,27 +6,7 @@ import { AppComponent } from '../components/index';
 @Component({
   // moduleId: module.id,
   selector: 'token',
-  template: `<div class="se-pre-con"></div>`,
-  styles: [ `.no-js #loader {
-      display: none;
-    }
-
-    .js #loader {
-      display: block;
-      position: absolute;
-      left: 100px;
-      top: 0;
-    }
-
-    .se-pre-con {
-      position: fixed;
-      left: 0px;
-      top: 0px;
-      width: 100%;
-      height: 100%;
-      z-index: 9999;
-      background: url(assets/image/loading-icon/loader-64x/Preloader_2.gif) center no-repeat #fff;
-    }` ]
+  template: `<div></div>`,
 })
 export class TokenComponent implements OnInit {
     name : any;
