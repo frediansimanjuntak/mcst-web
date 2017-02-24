654684ed3443aad0b9d2c5d2e3e83d776f7d20fd
Elic <rebellious.licxz@gmail.com>
2017-02-24 15:34:27 +0700

    Fixed contract error FIxed incident and payment reminder UI

--- app/components/contract/contract.component.ts
+++ app/components/contract/contract.component.ts
@@ -115,60 +115,55 @@ export class ContractComponent implements OnInit  {
         });
     }
 
-    deleteContractNote(contractnote: any , id:any) {
+    deleteContractNote(contractnoteid: any , id:any) {
         this.appComponent.loading = true
-        this.contractnoteService.delete(id,contractnote._id)
+        this.contractnoteService.delete(id,contractnoteid)
         .then(
-            response => {
-                if(response) {
-                    console.log(response);
+            data => {
+                    this._notificationsService.success(
+                            'Success',
+                            'Delete Contract successful',
+                    )
+                    this.ngOnInit();
+                },
+                error => {
+                    console.log(error);
                     this._notificationsService.error(
                             'Error',
                             'The Contract could not be deleted, server Error',
                     )
-                    this.appComponent.loading = false;
-                } else {
-                    this._notificationsService.success(
-                            'Success',
-                            'Delete Contarct successful',
-                    )
-                    this.contractService.getById(id)
-                    .subscribe(contract => {
-                        this.contract = contract;
-                        this.images = [];
-                        for (var i = 0; i < this.contract.attachment.length; ++i) {
-                            this.images.push({source:this.contract.attachment[i].url});
-                        };
-                    });
-                    this.contractnoteService.getAll(id)
-                    .subscribe(contractnotes => {
-                        if(contractnotes[0].contract_note.length > 0) { 
-                            this.contractnotes = contractnotes[0].contract_note;
-                        }
-                    })
-                    this.contractnoticeService.getAll(id)
-                    .subscribe(contractnotices => {
-                        if(contractnotices[0].contract_notice.length > 0) {
-                            this.contractnotices = contractnotices[0].contract_notice;
-                            setTimeout(() => this.appComponent.loading = false, 1000);
-                        }
-                    })
-                }
-            },
-            error=> {
-                console.log(error);
-                this._notificationsService.error(
-                            'Success',
-                            'The Contract could not be deleted, server Error',
-                )
+                    setTimeout(() => this.appComponent.loading = false, 1000);
+            //     }
+            // response => {
+            //     if(response) {
+            //         console.log(response);
+            //         this._notificationsService.error(
+            //                 'Error',
+            //                 'The Contract could not be deleted, server Error',
+            //         )
+            //         this.appComponent.loading = false;
+            //     } else {
+            //         this._notificationsService.success(
+            //                 'Success',
+            //                 'Delete Contarct successful',
+            //         )
+            //         this.ngOnInit();
+            //     }
+            // },
+            // error=> {
+            //     console.log(error);
+            //     this._notificationsService.error(
+            //                 'Success',
+            //                 'The Contract could not be deleted, server Error',
+            //     )
             }
         );
     }
 
     deleteContractNoteConfirmation(contractnote: any , id:any) {
+        console.log(contractnote , id)
         this.confirmationService.confirm({
             message: 'Are you sure that you want to delete this project note?',
-            header: 'Delete Confirmation',
             icon: 'fa fa-trash',
             accept: () => {
                 this.deleteContractNote(id,contractnote._id)
