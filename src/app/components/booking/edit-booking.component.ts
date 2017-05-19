f2cd5f9efc30a89a2f2b57b1de2847f7558619ff
Unknown <rebellious.licxz@gmail.com>
2017-05-19 09:49:47 +0700

    Fix booking error

--- src/app/components/booking/edit-booking.component.ts
+++ src/app/components/booking/edit-booking.component.ts
@@ -49,6 +49,13 @@ import  * as _  from "underscore";
 	table.tableSection td {
 		height: 50px
 	}
+	#booking-system p.row-li-2 {
+	    float: none !important;
+	}
+	#booking-system .abc {
+	    width: 100% !important;
+	}
+
   `]
 })
 
